f75df4fe1de015846401f6e3f60b38959149551b
Nicolas Ribeyrolle <nicolas.ribeyrolle@epita.fr>
2018-04-22 17:42:54 +0200

    SQL: modification of table (adding new fields)

--- src/user_settings/models.py
+++ src/user_settings/models.py
@@ -2,14 +2,14 @@ from django.db import models
 # Django's datetime supports timezones defined in settings.py
 from django.utils import timezone
 
+
 # Create your models here.
 # TODO move models to their related Django app.
-# FIXME incription date and if intern/extern
 class User(models.Model):
     firstname = models.CharField(max_length=100)
     lastname = models.CharField(max_length=100)
     mail = models.EmailField(max_length=100)
-    mail_secondary = models.EmailField(max_length=100, blank=True) # Blank is preferred. Do not use null.
+    mail_secondary = models.EmailField(max_length=100, blank=True)  # Blank is preferred. Do not use null.
     MALE = 'M'
     FEMALE = 'F'
     NON_BINARY = 'N'
@@ -19,6 +19,8 @@ class User(models.Model):
         (NON_BINARY, 'Non binaire'),
     )
     gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
+    inscription_date = models.DateTimeField()
+    is_intern = models.BooleanField()
 
     class Meta:
         verbose_name = "Utilisateur"
@@ -41,7 +43,7 @@ class Members(models.Model):
     def __str__(self):
         return "{0} fait partie de l'association {1}".format(self.user_id, self.association_id)
 
-#FIXME STATUT juridique
+
 class Association(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True)
@@ -49,19 +51,22 @@ class Association(models.Model):
     mail = models.EmailField(max_length=100)
     photo = models.ImageField()
     site = models.URLField()
+    statut = models.CharField(max_length=50)
 
-# FIXME deadline , size interne, size externe, token staff
+
 class Event(models.Model):
     association_id = models.ForeignKey(Association, on_delete=models.DO_NOTHING)
     title = models.CharField(max_length=100)
     date_begin = models.DateTimeField()
     date_end = models.DateTimeField()
+    date_deadline = models.DateTimeField()
     validated = models.BooleanField()
     description = models.TextField(blank=True)
-    price = models.DecimalField(decimal_places=2) # TODO find a better way to store money fields
+    price = models.DecimalField(decimal_places=2)  # TODO find a better way to store money fields
     place = models.CharField(max_length=100)
     photo = models.ImageField()
-    size = models.IntegerField()
+    size_intern = models.IntegerField()
+    size_extern = models.IntegerField()
     PREMIUM = 'PR'
     PAID = 'PA'
     FREE = 'FR'
@@ -72,14 +77,15 @@ class Event(models.Model):
 
     )
     priority = models.CharField(max_length=2, choices=PRIORITY)
+    token_staff = models.UUIDField()
 
 
-# FIXME hashcode of the ticket
 class Attend(models.Model):
     user_id = models.ForeignKey(User, on_delete=models.DO_NOTHING)
     event_id = models.ForeignKey(Event, on_delete=models.DO_NOTHING)
     date_entry = models.DateTimeField()
     ticket_number = models.UUIDField()
+
 
 class Staff(models.Model):
     user_id = models.ForeignKey(User, on_delete=models.DO_NOTHING)
@@ -87,15 +93,10 @@ class Staff(models.Model):
     date_begin = models.DateTimeField()
     date_end = models.DateTimeField()
 
+
 class SystemAdmin(models.Model):
     user_id = models.CharField(max_length=100)
 
+
 class AssociationsManager(models.Model):
     user_id = models.CharField(max_length=100)
-
-
-
-
-
-
-
