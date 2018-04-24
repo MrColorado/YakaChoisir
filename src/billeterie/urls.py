c2e6b82c7234a721f43f6a864c4a9adc4c219a0c
Nicolas Ribeyrolle <nicolas.ribeyrolle@epita.fr>
2018-04-24 15:59:04 +0200

    [AUTH] adding basic authenfication login and logout

--- src/billeterie/urls.py
+++ src/billeterie/urls.py
@@ -14,12 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from database import views as database_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('', database_views.home, name='home'),
     path('settings/', database_views.user_settings, name='settings'),
     path('event/', database_views.event, name='event'),
--- src/database/admin.py
+++ src/database/admin.py
@@ -1,2 +1,13 @@
 from django.contrib import admin
 
+from .models import *
+
+admin.site.register(User)
+admin.site.register(SystemAdmin)
+admin.site.register(AssociationsManager)
+admin.site.register(Association)
+admin.site.register(Members)
+admin.site.register(Event)
+admin.site.register(Attend)
+admin.site.register(Staff)
+
