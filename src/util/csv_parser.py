22e13ed51219139cc3a1f553a7d86b6125a72e29
Rod Guillaume <rodguillaume@tuta.io>
2018-04-25 18:49:46 +0200

    [CSV] add to_database() in CSVParser

--- src/util/csv_parser.py
+++ src/util/csv_parser.py
@@ -2,6 +2,9 @@ import csv
 import os
 
 
+# from src.databasIe.models import Association#, Members, User
+
+
 class CSVParser:
     """
     Excel CSV file parser.
@@ -38,21 +41,43 @@ class CSVParser:
         """
         res = ""
         for line in self.entries:
-            res += "{asso} - {statut} : {nom} {prenom} est {fonction} ({mail}) ({mailPerso})".format(
+            res += "{asso} - {statut} : {nom} {prenom} est {fonction} ({mailIonis}) ({mailPerso})".format(
                 asso=line[self.asso],
                 statut=line[self.statut],
                 nom=line[self.nom],
                 prenom=line[self.prenom],
                 fonction=line[self.fonction],
-                mail=line[self.mailIonis],
-                mailPerso=line[self.mailIonis]
+                mailIonis=line[self.mailIonis],
+                mailPerso=line[self.mailPerso]
             )
             res += os.linesep
         return res
 
-    def ToDataBase(self):
+    def to_database(self):
         """
         Fills up Django database with provided association and members.
         """
-        with open(self.filename) as csvfile:
-            reader = csv.DictReader(csvfile, dialect='excel')
+        res = ""
+        current_asso = None
+        for line in self.entries:
+            # Association line
+            if line[self.asso] != "":
+                current_asso = line[self.asso]
+
+                res += "New Association {name} {statut} {mail} would have been created\n".format(
+                    name=line[self.asso],
+                    statut=line[self.statut],
+                    mail=line[self.mailIonis] if line[self.mailIonis] != "" else line[self.mailPerso]
+                )
+            # Member line
+            if line[self.nom] != "":
+                res += "New {asso} member:\n\t{prenom} {nom} est un {role} et est joignable a {mail} ou "\
+                       "{mailSecondaire}".format(
+                        asso=current_asso,
+                        prenom=line[self.prenom],
+                        nom=line[self.nom],
+                        role=line[self.fonction],
+                        mail=line[self.mailIonis],
+                        mailSecondaire=line[self.mailPerso]
+                        )
+        return res
