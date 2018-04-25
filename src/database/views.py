4a6a74d0d874f5775b40fc14d5fe7b9d7b212005
issarn_t <theoissarni@hotmail.fr>
2018-04-25 20:02:29 +0200

    [EVENT] working on giving the right event

--- src/database/views.py
+++ src/database/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from database.models import Event
 
+
 # Create your views here.
 
 def home(request):
@@ -11,12 +12,15 @@ def user_settings(request):
     return render(request, 'user_settings/user_settings.html')
 
 
-#def event(request):
+# def event(request):
 #    return render(request, 'event/event.html')
 
 def event(request):
     events = Event.objects.all()
     return render(request, 'page_event/page_event.html', {'events': events})
 
-def specific_event(request):
-    return render(request, 'events/specific_event.html')
+
+def specific_event(request, Myid):
+    res_event = Event.objects.filter(id=Myid)
+    #res_event = get_object_or_404(Event, Myid)
+    return render(request, 'events/specific_event.html', {'res_event': res_event})
