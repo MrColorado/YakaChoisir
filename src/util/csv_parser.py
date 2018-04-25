b3621e6a75caa0091523945778bbea55b5c541a1
Rod Guillaume <rodguillaume@tuta.io>
2018-04-25 17:51:18 +0200

    [CSV] update CSVParser class so it does not open file everytime it uses it

--- src/util/csv_parser.py
+++ src/util/csv_parser.py
@@ -9,7 +9,14 @@ class CSVParser:
     """
 
     def __init__(self, filename):
+        """
+        Create a new CSVParser object.
+        :param filename: path to CSV file
+        """
+
         self.filename = filename
+        self.entries = []
+
         with open(self.filename, "r") as csvfile:
             reader = csv.DictReader(csvfile, dialect='excel')
 
@@ -22,19 +29,30 @@ class CSVParser:
             self.mailPerso = reader.fieldnames[6]
             self.mailIonis = reader.fieldnames[7]
 
-    def __str__(self):
-        str = ""
-        with open(self.filename, "r") as csvfile:
-            reader = csv.DictReader(csvfile, dialect='excel')
             for line in reader:
-                str += "{asso} - {statut} : {nom} {prenom} est {fonction} ({mail}) ({mailPerso})".format(
-                    asso=line[self.asso],
-                    statut=line[self.statut],
-                    nom=line[self.nom],
-                    prenom=line[self.prenom],
-                    fonction=line[self.fonction],
-                    mail=line[self.mailIonis],
-                    mailPerso=line[self.mailIonis]
-                )
-                str += os.linesep
-        return str
+                self.entries.append(line)
+
+    def __str__(self):
+        """
+        :return: CSV file parsed as a string
+        """
+        res = ""
+        for line in self.entries:
+            res += "{asso} - {statut} : {nom} {prenom} est {fonction} ({mail}) ({mailPerso})".format(
+                asso=line[self.asso],
+                statut=line[self.statut],
+                nom=line[self.nom],
+                prenom=line[self.prenom],
+                fonction=line[self.fonction],
+                mail=line[self.mailIonis],
+                mailPerso=line[self.mailIonis]
+            )
+            res += os.linesep
+        return res
+
+    def ToDataBase(self):
+        """
+        Fills up Django database with provided association and members.
+        """
+        with open(self.filename) as csvfile:
+            reader = csv.DictReader(csvfile, dialect='excel')
