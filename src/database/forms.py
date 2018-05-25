511735f162725452442579deb5a8f31532d13d74
Lene Julien <julien.lene@epita.fr>
2018-05-25 20:27:14 +0200

    [create_event] write the beginning of the create event function

--- src/database/forms.py
+++ src/database/forms.py
@@ -4,6 +4,8 @@ from database.models import myUser
 
 
 class createEventForm(forms.Form):
+
+    #if forms.is_valid() :
     asso_list = []
     for a in Association.objects.all():
         asso_list.append((a.id, a.name))
@@ -31,6 +33,35 @@ class createEventForm(forms.Form):
     photo = forms.ImageField(widget=forms.FileInput(attrs={'type': 'file',
                                                            'class': 'form-control-file'}))
 
+class modifyEventForm(forms.Form):
+    def __init__(self, *args, **kwargs):
+        self.res_event = kwargs.pop(self, 'res_event')
+        super(modifyEventForm, self).__init__(*args, **kwargs)
+        self.fields['title'].widget = forms.TextInput(attrs={'class': 'form-control',
+                                                          'value': self.res_event})
+
+
+    #association_name = forms.Textarea(widget=forms.Textarea(attrs={'class': 'form-control'
+     #                                                              }))
+
+    title = forms.CharField()
+    description = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control'}))
+    price = forms.DecimalField(widget=forms.NumberInput(attrs={'class': 'form-control',
+                                                               'value': '0'}))
+    description = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control'}))
+    place = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
+    size_intern = forms.IntegerField(widget=forms.NumberInput(attrs={'class': 'form-control',
+                                                                     'value': '100'}))
+    size_extern = forms.IntegerField(widget=forms.NumberInput(attrs={'class': 'form-control',
+                                                                     'value': '100'}))
+    date_begin = forms.DateTimeField(widget=forms.DateTimeInput(attrs={'class': 'form-control',
+                                                                       'type': 'datetime-local'}))
+    date_end = forms.DateTimeField(widget=forms.DateTimeInput(attrs={'class': 'form-control',
+                                                                     'type': 'datetime-local'}))
+    date_deadline = forms.DateTimeField(widget=forms.DateTimeInput(attrs={'class': 'form-control',
+                                                                          'type': 'datetime-local'}))
+    photo = forms.ImageField(widget=forms.FileInput(attrs={'type': 'file',
+                                                           'class': 'form-control-file'}))
 
 class createAssociationForm(forms.Form):
     name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
--- src/database/views/user_views.py
+++ src/database/views/user_views.py
@@ -24,6 +24,18 @@ def user_information(request):
     return render(request, 'user_settings/user_settings.html', {'user_info': user_info})
 
 
+@login_required
+def modifyUserinfo(request):
+    print("yolo")
+
+    if request.method == 'POST':
+
+        form = modifyUser(request.POST)
+        user_to_modify = myUser.objects.get(user=request.user)
+        user_to_modify.mail_secondary = form.data['secondary_email']
+        user_to_modify.save()
+
+
 def user_modify(request):
     user_to_modify = myUser.objects.get(user=request.user)
     print(user_to_modify)
@@ -34,7 +46,26 @@ def user_modify(request):
             user_to_modify.mail_secondary = form.data['mail_secondary']
         if form.data['gender'] != "":
             user_to_modify.gender = form.data['gender']
+            user_to_modify.save()
+            return render(request, 'user_settings/user_settings.html', {'user_info': user_to_modify})
+        else:
+            return render(request,'not_found.html')
+    else:
+        form = modifyV2(request.POST)
+        return render(request, 'user_settings/user_settings.html', {'form': form})
+'''<<<<<<< HEAD
         user_to_modify.save()
         return render(request, 'user_settings/user_settings.html', {'user_info': user_to_modify})
     form = modifyUser()
     return render(request, 'user_settings/modify_user.html', locals(), {'user_info': user_to_modify})
+======='''
+#@login_required
+#def is_god(request):
+#    user_info = myUser.objects.get(user=request.user)
+#    members = Members.objects.filter(user_id=user_info)
+#    god = False
+#    print (SystemAdmin.objects.filter(user_id=user_info))
+#    if (SystemAdmin.objects.filter(user_id=user_info) or AssociationsManager.objects.filter(user_id=user_info)):
+#        god = True
+#
+#    return render(request, 'event/my_event.html', {'god': god})
