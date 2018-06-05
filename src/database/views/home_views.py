f7bb6cfaaa6bf546927a554e355d830b6880926d
Lene Julien <julien.lene@epita.fr>
2018-06-05 21:05:51 +0200

    [INDEX] fix date on prenium event

--- src/database/views/home_views.py
+++ src/database/views/home_views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.utils import timezone
 
 
 from database.models import Event
@@ -12,11 +13,13 @@ def home(request):
     allEvents = Event.objects.all()
     cpt = 0
     for e in allEvents:
-        if cpt == 0:
+        if cpt == 0 and e.premium and e.date_begin > timezone.now():
             first.append(e)
-        if e.validated and e.premium and cpt > 0:
+            cpt = cpt + 1
+            continue
+        if e.validated and e.premium and cpt > 0 and e.date_begin > timezone.now():
             events.append(e)
             numbers.append(cpt)
-        cpt = cpt + 1
+            cpt = cpt + 1
 
     return render(request, 'home/index.html', {'events': events, 'first': first, 'numbers': numbers})
