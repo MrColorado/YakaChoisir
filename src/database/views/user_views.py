bce87b02efee34064e1512f01b25b8546eccd992
Lene Julien <julien.lene@epita.fr>
2018-06-08 15:11:58 +0200

    [MERGE] conflit in urls and user_views

--- src/database/views/user_views.py
+++ src/database/views/user_views.py
@@ -8,33 +8,20 @@ from database.models import SystemAdmin
 from database.forms import *
 
 
-@login_required
-def is_god(request):
-    user_info = myUser.objects.get(user=request.user)
-    members = Members.objects.filter(user_id=user_info)
-    god = False
-    print(SystemAdmin.objects.filter(user_id=user_info))
-    if (SystemAdmin.objects.filter(user_id=user_info) or AssociationsManager.objects.filter(user_id=user_info)):
-        god = True
-    return render(request, 'event/my_event.html', {'god': god})
-
-
 def user_information(request):
     user_info = myUser.objects.get(user=request.user)
     return render(request, 'user_settings/user_settings.html', {'user_info': user_info})
 
-
+'''
 @login_required
 def modifyUserinfo(request):
-    print("yolo")
-
     if request.method == 'POST':
 
         form = modifyUser(request.POST)
         user_to_modify = myUser.objects.get(user=request.user)
         user_to_modify.mail_secondary = form.data['secondary_email']
         user_to_modify.save()
-
+'''
 
 def user_modify(request):
     user_to_modify = myUser.objects.get(user=request.user)
@@ -48,17 +35,16 @@ def user_modify(request):
             user_to_modify.gender = form.data['gender']
             user_to_modify.save()
             return render(request, 'user_settings/user_settings.html', {'user_info': user_to_modify})
-        else:
-            return render(request,'not_found.html')
-    else:
-        form = modifyV2(request.POST)
-        return render(request, 'user_settings/user_settings.html', {'form': form})
-'''<<<<<<< HEAD
         user_to_modify.save()
         return render(request, 'user_settings/user_settings.html', {'user_info': user_to_modify})
     form = modifyUser()
     return render(request, 'user_settings/modify_user.html', locals(), {'user_info': user_to_modify})
-======='''
+'''        else:
+            return render(request,'not_found.html')
+    else:
+        form = modifyV2(request.POST)
+        return render(request, 'user_settings/user_settings.html', {'form': form})'''
+
 #@login_required
 #def is_god(request):
 #    user_info = myUser.objects.get(user=request.user)
