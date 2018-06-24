ac255b85998436d1f40bc57e50ae68d3a2713228
Rod Guillaume <rodguillaume@tuta.io>
2018-06-24 17:12:13 +0200

    [MISC] remove trailing whitespaces in ALLOWED HOSTS

--- src/billeterie/settings.py
+++ src/billeterie/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'h%!eq=h1$zk=s1a4hrul8@(-ub)xws4ta7m85e^tfld!f6w-#c'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [ '*' ]
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
