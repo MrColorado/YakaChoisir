8147e7241ed81e9e285b05ceb390564aefbf030f
Rod Guillaume <rodguillaume@tuta.io>
2018-06-27 02:24:40 +0200

    [APP] add QRCode Scanner Fragment

--- android/app/src/main/java/com/yakapayer/yakascanner/MainActivity.kt
+++ android/app/src/main/java/com/yakapayer/yakascanner/MainActivity.kt
@@ -5,12 +5,14 @@ import android.support.v7.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
+    val test = "salut"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val fragmentTransaction = supportFragmentManager.beginTransaction()
-        fragmentTransaction.replace(R.id.main_container, ChooseEventFragment())
+        fragmentTransaction.replace(R.id.main_container, ChooseEventFragment()).addToBackStack("MAIN_FRAGMENT")
 
         fragmentTransaction.commit()
     }
